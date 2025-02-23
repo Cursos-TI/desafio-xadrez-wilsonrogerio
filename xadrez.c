438f7e65b241907974bfd30c31f6ea0e98198809
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2025-02-23 13:03:39 +0000

    adicionado movimentos nivel Basico

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,42 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+       //movimento da torre
+       int x = 0;
+       do
+       {   
+           printf("Torre - Diretira\n");
+           x++;  
+           /* code */
+       } while (x < 5);
+       printf("\n");
+       
+       //movimento do bispo
+       int y = 0;
+       while (y < 5)
+       {
+           printf("Bispo - Cima,Direita\n");
+           y++;
+       }
+       printf("\n");
+   
+       // movimento da rainha
+       int i = 0;
+       for ( i = 0; i < 8; i++)
+       {
+           printf("Rainha - Esquerda\n");
+       }
+       
+       printf("\n");
+       // movimento do cavalo
+      do
+      {
+       for ( i = 0; i < 2; i++)
+       {
+           printf("Cavalo - Cima\n");
+       }
+       printf("Cavalo - Direita\n");
+      
+      } while (i < 1);
+      return 0;
 }
